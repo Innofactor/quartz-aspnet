bce22892cf2acbcf375d53a4e774c05e9ba85b95
HEADQUARTERS\jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2018-03-20 12:50:11 +0200

    initial commit

---
+++ Innofactor.QuartzAspNet/BatchJobFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Spi;
+using System;
+using System.Threading.Tasks;
+
+namespace Innofactor.QuartzAspNet {
+
+  public class BatchJobFactory : IJobFactory {
+    private readonly IServiceProvider _serviceProvider;
+
+    public BatchJobFactory(IServiceProvider serviceProvider) {
+      _serviceProvider = serviceProvider;
+    }
+
+    public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler) {
+
+      var scope = _serviceProvider.CreateScope();
+      var jobDetail = bundle.JobDetail;
+      var actualJob = (IJob)scope.ServiceProvider.GetService(jobDetail.JobType);
+      return new ScopedJob(actualJob, scope);
+
+    }
+
+    public void ReturnJob(IJob job) {
+      //TODO: Clean up logic if needed
+    }
+  }
+
+  public class ScopedJob : IJob {
+
+    private IJob actualJob;
+    private IServiceScope scope;
+
+    public ScopedJob(IJob actualJob, IServiceScope scope) {
+      this.actualJob = actualJob;
+      this.scope = scope;
+    }
+
+    public async Task Execute(IJobExecutionContext context) {
+      await actualJob.Execute(context);
+      scope.Dispose();
+    }
+  }
+
+}
---
+++ Innofactor.QuartzAspNet/QuartzExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Innofactor.QuartzAspNet {
+
+  public static class QuartzExtensions {
+
+    public static void UseQuartz(this IServiceCollection services, string connectionString,
+      IEnumerable<Type> jobs) {
+
+      services.AddSingleton<IJobFactory, BatchJobFactory>();
+      services.Add(jobs.Select(jobType => new ServiceDescriptor(jobType, jobType, ServiceLifetime.Transient)));
+
+      services.AddSingleton(provider => {
+
+        var props = new NameValueCollection {
+          { "quartz.serializer.type", "json" }
+        };
+        var schedulerFactory = new StdSchedulerFactory(props);
+        var scheduler = schedulerFactory.GetScheduler().Result;
+        scheduler.JobFactory = provider.GetService<IJobFactory>();
+        scheduler.Clear();
+        scheduler.Start();
+
+        return scheduler;
+      });
+
+    }
+
+    public static void StartJob<TJob>(this IScheduler scheduler, ITrigger trigger)
+      where TJob : IJob {
+      var jobName = typeof(TJob).Name;
+
+      var job = JobBuilder.Create<TJob>()
+        .WithIdentity(jobName)
+        .Build();
+
+      scheduler.ScheduleJob(job, trigger);
+    }
+
+  }
+
+}
